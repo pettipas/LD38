e1a3abe6196eac3868d74b64a2be8649db5e6183
DESKTOP-5UOFQ43\paulpettipas <paul.pettipas@gmail.com>
2017-04-22 17:54:49 -0300

    ok spider uses a nav mesh to chase blit on screen

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public Animator animator;
+    public float distance = 2.0f;
+    public float coolDown = 3.0f;
+    public float timer; 
+
+    public void Update() {
+        timer += Time.smoothDeltaTime;
+        if (Vector3.Distance(transform.position, Game.instance.UserPosition) < distance && timer >= coolDown) {
+            timer = 0;
+            animator.SafePlay("spider_attack");
+        }
+
+        if (timer < coolDown) {
+            agent.SetDestination(transform.position);
+            return;
+        }
+
+        agent.SetDestination(Game.instance.UserPosition);
+        animator.SafePlay("spider_walk");
+    }
+}
--- Assets/Game.cs
+++ Assets/Game.cs
@@ -15,18 +15,17 @@ public class Game : MonoBehaviour {
         instance = this;
     }
 
+    public Vector3 UserPosition {
+       get { return curser.transform.position; }
+    }
+
     public void Update() {
         Vector3 p = player.transform.position/5.0f;
-    
         curser.transform.position = new Vector3(p.x * 128.0f, 5, p.z * 128.0f);
     }
 
     public void OnProjectileAtMaxRange(Projectile projectile) {
         projectile.enabled = false;
         projectile.transform.position = gun.transform.position;
-    }
-
-    public void OnGUI() {
-        GUILayout.Label(player.transform.localPosition.ToString());
     }
 }
